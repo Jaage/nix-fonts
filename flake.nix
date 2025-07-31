95b1ba13ec3c3ae6d203fe53ef1db8465c64c721
Jaage <jordanjhubbard@gmail.com>
2025-07-31 15:22:08 -0700

    fix: maybe path is wrong?

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           };
           installPhase = ''
             mkdir -p $out/share/fonts/truetype
-            cp -R $src $out/share/fonts/truetype
+            cp -R $src $out/share/fonts/truetype/
           '';
           meta = {
             description = "The Runescape Font Family derivation.";
