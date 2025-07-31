75b18799081ef7e2dd55c14655398d1c6a37b3b4
Jaage <jordanjhubbard@gmail.com>
2025-07-31 14:14:24 -0700

    feat: don't unpack .ttf

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A flake that installs fonts not in Nixpkgs.";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        defaultPackage = pkgs.symlinkJoin {
+          name = "myfonts-0.1.0";
+          paths = builtins.attrValues self.packages.${system}; # Add font derivation names here
+        };
+
+        packages.runescape = pkgs.stdenvNoCC.mkDerivation {
+          name = "runescape-font";
+          dontUnpack = true;
+          src = pkgs.fetchurl {
+            url = "https://github.com/runelite/runelite/blob/master/runelite-client/src/main/resources/net/runelite/client/ui/runescape.ttf";
+            sha256 = "sha256-GNOAfqJ+wSPO6HBsMIiu7XyDlNmms8My+YQP8BrsxxM=";
+          };
+          installPhase = ''
+            mkdir -p $out/share/fonts/truetype
+            cp -R $src $out/share/fonts/truetype
+          '';
+          meta = {
+            description = "The Runescape Font Family derivation.";
+          };
+        };
+      }
+    );
+}
